173e06cf63ca248aa113abf37f7c6f11d6b1429c
azelalynetan <azelalynetan@gmail.com>
2023-05-21 23:18:17 +0800

    Add template for og image

---
+++ components/islands/SampleOgImage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    :style="{
+      position: 'relative',
+      backgroundColor: '#aacaef',
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      padding: '60px',
+    }"
+  >
+    <div
+      :style="{
+        height: '100%',
+        paddingBottom: '120px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+      }"
+    >
+      <div
+        :style="{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          maxWidth: '680px',
+        }"
+      >
+        <p
+          :style="{
+            fontSize: '72px',
+            fontWeight: '700',
+            marginTop: '0',
+            marginBottom: '10px'
+          }"
+        >
+          {{ name }}
+        </p>
+        <p
+          :style="{
+            fontSize: '40px',
+            fontWeight: '400',
+            margin: '0',
+            textAlign: 'center',
+          }"
+        >
+          {{ description }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  name: String,
+  description: String,
+});
+</script>
